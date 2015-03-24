8654bc0540dde98e6d60e84185b083311628bb95
Komil <open@code.tj>
2015-03-24 11:16:08 +0500

    changes in app conf

--- app/conf.php
+++ app/conf.php
@@ -1,10 +1,10 @@
 <?php
 // constants
 define('BDIR',str_replace('\\','/',realpath(dirname(__FILE__)))); // absolute base dir path for app
-define('CDIR','/home/iw7/web/nihol2/core'); // abs core dir path
+define('CDIR','/home/nihol2/core'); // abs core dir path
 define('APPDIR',BDIR.'/app'); // path to application directory
 define('APPATH','./app'); // here path means url not dir
-define('APPNAME','bweb2'); // application codename
+define('APPNAME','test'); // application codename
 define('APPREFIX',APPNAME.'_'); // prefix for session vars
 define('UIPATH','./ui');
 define('XMODE',0); // maintenance
@@ -14,18 +14,7 @@ $conf['db_server']='localhost';
 $conf['db_port']='';
 $conf['db_charset']='utf8';
 $conf['db_name']=''.APPNAME;
-$conf['db_user']='bweb2';
-$conf['db_pass']='Test2014';
+$conf['db_user']='test';
+$conf['db_pass']='test';
 // user interface
-$conf['tpl']=UIPATH.'/tpl/bweb'; // template dir
-// additional app settings
-$conf['breports']='/home/iw7/web/breports';
-
-// modules registry, 1 - core, 2 - app
-$xmods=array(
-'user'=>1,
-'groups'=>1,
-'page'=>1,
-'tb'=>2,
-'br'=>2,
-);
+$conf['tpl']=UIPATH.'/tpl/test'; // template dir
